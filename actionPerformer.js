9a0f13651577f4bd435b12afe3826f6d1464f93d
Sudhin <sudhinmachad@gmail.com>
2017-12-18 16:44:47 +0530

    clicks is ok but we cant use timeout

--- actionPerformer.js
+++ actionPerformer.js
@@ -1,16 +1,16 @@
-// const controller=require('./confuseController.js');
+
 let count=0;
 const colorChecker=function(){
-  // let grid=document.getElementById('grid');
-  document.getElementById('grid').addEventListener('click',clickChecker);
-  // grid.onclick=function(event) {
-    // let displayColor=document.getElementById('colorName');
-    // // setTimeout(function(){displayColor.style.display='none'},3000);
-    //
-    // let color=getColor(event.target.id);
-    // displayColor.innerText=color[0];
-    // setTimeout(function(){displayColor.innerText='Game Over'},3000);
-  // }
+  let grid=document.getElementById('grid');
+  // document.getElementById('grid').addEventListener('click',clickChecker);
+  grid.onclick=function(event) {
+    let displayColor=document.getElementById('colorName');
+    // setTimeout(function(){displayColor.style.display='none'},3000);
+
+    let color=getColor(event.target.id);
+    displayColor.innerText=color[0];
+    setTimeout(function(){displayColor.innerText='Game Over'},3000);
+  }
 };
 
 const clickChecker=function(){
